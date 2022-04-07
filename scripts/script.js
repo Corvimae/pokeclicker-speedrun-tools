33b7a121d766a8a36952005bdd92cdcf7bc1d453
May <may@maybreak.com>
2022-04-07 00:09:36 -0500

    style

--- scripts/script.js
+++ scripts/script.js
@@ -14,24 +14,31 @@ function enableAutoclicker() {
       // Wait until the starter pokemon has been caught
       if (App.game.party.caughtPokemon.length === 0) {
         autoclickerStatusElem.textContent = `Auto-clicker enabled, awaiting first catch...`;
+
         return;
       }
 
       // Pause while modals are open
       if (document.querySelectorAll('.modal.show').length >= 1) {
         autoclickerStatusElem.textContent = `Auto-clicker paused, modal open...`;
+
         return;
       }
       
       // Route
-      if (App.game.gameState == GameConstants.GameState.fighting && Battle.enemyPokemon() && Battle.enemyPokemon().health() > 0)
+      if (App.game.gameState == GameConstants.GameState.fighting && Battle.enemyPokemon() && Battle.enemyPokemon().health() > 0) {
         Battle.clickAttack();
+      }
+
       // Gym
-      if (App.game.gameState == GameConstants.GameState.gym && GymBattle.enemyPokemon() && GymBattle.enemyPokemon().health() > 0)
+      if (App.game.gameState == GameConstants.GameState.gym && GymBattle.enemyPokemon() && GymBattle.enemyPokemon().health() > 0) {
         GymBattle.clickAttack();
+      }
+
       // Dungeon
-      if (App.game.gameState == GameConstants.GameState.dungeon && DungeonBattle.enemyPokemon() && DungeonBattle.enemyPokemon().health() > 0)
+      if (App.game.gameState == GameConstants.GameState.dungeon && DungeonBattle.enemyPokemon() && DungeonBattle.enemyPokemon().health() > 0) {
         DungeonBattle.clickAttack();
+      }
 
       autoclickerStatusElem.textContent = `Auto-clicker enabled at ${AUTOCLICKER_INTERVAL_MS}ms.`;
     }, AUTOCLICKER_INTERVAL_MS);
