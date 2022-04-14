892de2c1e33b6054dcb5d37c803a35c91e2a4edb
May Roussel <may@maybreak.com>
2022-04-14 10:55:38 -0500

    Add a button to delete all generic saves.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Makes speedrunning Pokeclicker nicer. Yay!
 ## Features
 
 - Auto-clicker at 100ms. By default, this is disabled; click the text in the bottom-right that gets added to the page to enable it. The auto-clicker will not start until you've caught your first Pokemon.
+- One-click option to delete all saves that have the name "Trainer". For safety, only saves that have fewer than 14 badges are ever deleted (since the game considers Elite Four clears as badges, this is all Kanto Gym Leaders and the Kanto Elite Four).
 
 ## Suggestions
 
--- scripts/script.js
+++ scripts/script.js
@@ -42,7 +42,6 @@ function enableAutoclicker() {
 
       autoclickerStatusElem.textContent = `Auto-clicker enabled at ${AUTOCLICKER_INTERVAL_MS}ms.`;
     }, AUTOCLICKER_INTERVAL_MS);
-
   }
 
   function stopAutoclicker() {
@@ -67,8 +66,42 @@ function enableAutoclicker() {
   stopAutoclicker();
 }
 
+function addDeleteGenericsButton() {
+  if (!document.querySelector('.delete-generic-runs')) {
+    const button = document.createElement('button');
+
+    button.classList.add('btn', 'btn-success', 'col-md-4', 'col-xs-12', 'delete-generic-runs');
+    button.textContent = 'Delete "Trainer" Saves';
+    button.onclick = () => {
+      const saveKeys = Object.keys(localStorage).filter(name => name.startsWith('save'));
+
+      saveKeys.forEach(key => {
+        try {
+          const saveData = JSON.parse(localStorage.getItem(key));
+
+          const username = saveData.profile?.name ?? 'Trainer';
+          const badgeCount = saveData.badgeCase?.filter(x => x).length ?? 0;
+
+          if (username === 'Trainer' && badgeCount < 14) {
+            localStorage.removeItem(key);
+          }
+        } catch (e) {
+          console.error(`Corrupted save data ${key}, skipping...`);
+          console.error(e);
+        }
+      });
+
+      document.querySelector('#saveSelector .save-container').innerHTML = '';
+      SaveSelector.loadSaves();
+    };
+
+    document.querySelector('.new-import-buttons').appendChild(button);
+  }
+}
+
 (() => {
   console.info('Pokeclicker Speedrun Tools enabled.');
 
   enableAutoclicker();
+  addDeleteGenericsButton();
 })();
